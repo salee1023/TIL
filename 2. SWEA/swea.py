82194e3b47be776d8fc0d1f273e46fd5a6a8ae5c
salee1023 <dltmddk1023@gmail.com>
2020-08-03 23:52:59 +0900

    0803 Today I Learned

--- 2. SWEA/swea.py
+++ 2. SWEA/swea.py
@@ -1,24 +1,35 @@
 case = int(input())
 
 for c in range(1, case+1):
-    # nxn 정사각형, m = 글자 수
     n , m = map(int, input().split())
-    array1 = []
-    array2 = []
+    total = 0
 
     # 2차원 list 생성
-    for t in range(n):
+    array1 = []
+    for _ in range(n):
         array1.append(list(map(int, input().split())))
-    # 행열 바꾼 array2 생성
-    
+
+    for numbers in array1:
+        if numbers.count(1) >= m:
+            count = 0
+            for i in range(n-m+1):
+                if numbers[i:i+m].count(1) == m:
+                        count += 1
+            if count == 1:
+                total += 1
+
+    # 행열 바꾼 array2 생성 
+    array2 = []
     for idx in range(n):
         array2.append([i[idx] for i in array1])
 
-    # array1, array2에 1이 연속 3번 나오면 result + 1
-    count = 0
-    for i in range(n):
-        # 아래는 갯수만 알려줄 뿐, 연속된 숫자가 아님.
-        if (array1[i].count(1) == 3)  or (array2[i].count(1) == 3):
-            count += 1  
-        # 결과 출력
-    print(f'#{t} {count}')
+    for numbers in array2:
+        if numbers.count(1) >= m:
+            count = 0
+            for i in range(n-m+1):
+                if numbers[i:i+m].count(1) == m:
+                        count += 1
+            if count == 1:
+                total += 1 
+    
+    print(f'#{c} {total}')
