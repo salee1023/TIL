b2ccc2bbaa90a921e972ce76a159754b2dfb7f06
salee1023 <dltmddk1023@gmail.com>
2020-08-06 23:36:58 +0900

    0806 Algorithm 수정

--- 3.Algorithm/array2/Carrot3.py
+++ 3.Algorithm/array2/Carrot3.py
@@ -1,22 +1,25 @@
 T = int(input())
 for tc in range(1,1+T):
-    N = int(input())
-    carrot = [list(map(int, input().split())) for _ in range(10)]
-    alarm = []
-    new
-    # 경보기 위치
-    for i in range(10):
-        for j in range(10):
-            if carrot[i][j] == 1:
-                alarm.append(i)
-                alarm.append(j)
-    alarm1 = alarm[0:2]
-    alarm2 = alarm[len(alarm)-2:]
-    # 멧돼지 확인 1
-    for i in range(0, alarm1[0]+1):
-        for j in
-
-
-
-
-
+    N, M = map(int, input().split())
+    carrot = [list(map(int, input().split())) for _ in range(N)]
+    result = 987654321
+    for i in range(N-1):
+        for j in range(M-1):
+            c = [0]*3
+            # 1번 일꾼
+            for x in range(i+1):
+                for y in range(0, j+1):
+                    c[0] += carrot[x][y]
+            # 2번 일꾼
+            for x in range(i+1):
+                for y in range(j+1, M):
+                    c[1] += carrot[x][y]
+            # 3번 일꾼
+            for x in range(i+1, N):
+                for y in range(M):
+                    c[2] += carrot[x][y]
+            # 최소 차이
+            diff = max(c)-min(c)
+            if diff< result:
+                result = diff
+    print(f'#{tc} {result}')
--- 3.Algorithm/array2/CarrotAlarm.py
+++ 3.Algorithm/array2/CarrotAlarm.py
@@ -1,33 +1,33 @@
 T = int(input())
 for tc in range(1,1+T):
     N = int(input())
-    carrot = [list(int, input().split()) for _ in range(10)]
-    cnt = 0
-    # 경보기 위치 파악하기
+    carrot = [list(map(int, input().split())) for _ in range(10)]
+    alarm = []
+    # 경보기 위치
     for i in range(10):
         for j in range(10):
-            # 첫번째 모서리
-            if carrot[i][j] == 0 and carrot[i][j+1] == 1 and carrot[i+1][j] == 1:
-                for x in range(0, i+1):
-                    for y in range(0, j+2):
-                        if carrot[x][y] == 2:
-                            cnt += 1
-            # 두번째 모서리
-            if carrot[i][j] == 1 and carrot[i][j+1] == 0 and carrot[i+1][j] == 1:
-                for x in range(0, i+1):
-                    for y in range(j+1, 10):
-                        if carrot[x][y] == 2:
-                            cnt += 1
-            # 세번째 모서리
-            if carrot[i][j] == 0 and carrot[i][j+1] == 1 and carrot[i+1][j] == 0:
-                for x in range(0, i+2):
-                    for y in range(0, j+1):
-                        if carrot[x][y] == 2:
-                            cnt += 1
+            if carrot[i][j] == 1:
+                alarm.append(i)
+                alarm.append(j)
+    alarm1 = alarm[0:2] # 왼쪽위 좌표
+    alarm2 = alarm[len(alarm)-2:] # 오른쪽아래 좌표
+    # 멧돼지 감지
+    cnt = 0
+    for x in range(0, alarm1[0]+1): # 1
+        for y in range(0, alarm1[1]+1):
+            if carrot[x][y] == 2:
+                cnt += 1
+    for x in range(0, alarm1[0]+1): # 2
+        for y in range(alarm2[1], 10):
+            if carrot[x][y] == 2:
+                cnt += 1
+    for x in range(alarm2[0], 10): # 3
+        for y in range(0, alarm1[1]+1):
+            if carrot[x][y] == 2:
+                cnt += 1
+    for x in range(alarm2[0], 10): # 4
+        for y in range(alarm2[1], 10):
+            if carrot[x][y] == 2:
+                cnt += 1
 
-
-
-
-
-
-
+    print(f'#{tc} {cnt}')
