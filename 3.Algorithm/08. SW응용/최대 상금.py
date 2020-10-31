548bd3cd6b98269f0eea628507e52597294840fa
salee1023 <dltmddk1023@gmail.com>
2020-11-01 02:21:58 +0900

    1101 Today I Learned

--- 3.Algorithm/08. SW응용/최대 상금.py
+++ 3.Algorithm/08. SW응용/최대 상금.py
@@ -1,33 +1,32 @@
-def find_idx(tmp):
-    for j in range(len(n) - 1, -1, -1):
-        if tmp == number[j]:
-            return j
+def exchange(x):
+    global my_max
+    new_num = int(''.join(number))
+    # x만큼 바꾼 숫자가 memo에 있으면 return
+    if new_num in memo[x]:
+        return
+    # memo에 없으면 memo[x]에 기록
+    memo[x].append(new_num)
+    # 주어진 횟수(cnt)만큼 숫자 자리 바꾸기
+    if x == cnt:
+        if new_num > my_max:
+            my_max = new_num
+        return
+    else:
+        for i in range(len(number)-1):
+            for j in range(i+1, len(number)):
+                number[i], number[j] = number[j], number[i]
+                exchange(x+1)
+                number[i], number[j] = number[j], number[i]
 
 #-------------------------------------------
 T = int(input())
 for tc in range(1, 1+T):
-    n, c = input().split()
-    number = []
-    c = int(c)
-    for i in range(len(n)):
-        number.append(int(n[i]))
-
-    i = 0
-    while c and i < len(n)-1:
-        tmp = max(number[i+1:])
-        tmp_i = find_idx(tmp)
-        if number[i] <= tmp:
-            number[i], number[tmp_i] = number[tmp_i], number[i]
-            c -= 1
-        # elif number[i] >= tmp:
-        #     c -= 1
-        i += 1
-    if c % 2:
-        number[-1], number[-2] = number[-2], number[-1]
-
-    print(f'#{tc} ', end='')
-    for x in number:
-        print(x, end='')
-    print()
-
-
+    number, cnt = input().split()
+    number = list(number)
+    cnt = int(cnt)
+    # 주어진 횟수 만큼 자리를 바꾼 숫자 기록
+    memo = [[] for _ in range(cnt+1)]
+    # 최대값
+    my_max = 0
+    exchange(0)
+    print(f'#{tc} {my_max}')
