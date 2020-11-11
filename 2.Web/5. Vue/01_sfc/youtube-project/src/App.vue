0aea8941e97377d187b4d0a222f89da8c6f66652
salee1023 <dltmddk1023@gmail.com>
2020-11-12 00:42:25 +0900

    1111 Today I Learned2

--- 2.Web/5. Vue/01_sfc/youtube-project/src/App.vue
+++ 2.Web/5. Vue/01_sfc/youtube-project/src/App.vue
@@ -3,7 +3,12 @@
     <h1>My First Youtube Project</h1>
     <!-- 3.보여준다. -->
     <SearchBar @input-change="onInputChange" />
-    <VideoList :videos="videos"/>
+    <!-- 16. selectedVideo 데이터를 video라는 이름으로 자식 컴포넌트(videoDetail)로 내려준다. -->
+    <VideoDetail :video="selectedVideo"/>
+    <!-- 7. :videos="videos" 응답을 받은 배열(비디오 목록)을 VideoList 컴포넌트로 내려준다. -->
+    <!-- 14. 자식 컴포넌트로부터 select-video 이벤트가 올라오면 onVideoSelect 메서드를 실행한다. -->
+    <VideoList :videos="videos" @select-video="onVideoSelect"/>
+    
   </div>
 </template>
 
@@ -13,6 +18,7 @@ import axios from 'axios'
 //1. 불러온다.
 import SearchBar from '@/components/SearchBar'
 import VideoList from '@/components/VideoList'
+import VideoDetail from '@/components/VideoDetail'
 
 const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY
 const API_URL = 'https://www.googleapis.com/youtube/v3/search'
@@ -26,22 +32,24 @@ export default {
     //2. 등록한다.
     SearchBar,
     VideoList,
+    VideoDetail,
   },
   data: function () {
     return {
       inputValue: '',
+      selectedVideo: '',
       videos: [],
     }
   },
   methods: {
     onInputChange: function (inputText) {
-      console.log('데이터가 SearchBar로부터 올라왔다!!!!')
-      console.log(inputText)
+      // console.log('데이터가 SearchBar로부터 올라왔다!!!!')
+      // console.log(inputText)
 
-      //1. 자식 컴포넌트(SearchBar.vue)로부터 올라온 데이터 inputValue라는 App.vue 컴포넌트 데이터에 넣는다.
+      //3. 자식 컴포넌트(SearchBar.vue)로부터 올라온 데이터 inputValue라는 App.vue 컴포넌트 데이터에 넣는다.
       this.inputValue = inputText
 
-      //2. Youtube API로 요청을 위한 준비를 한다.
+      //4. Youtube API로 요청을 위한 준비를 한다.
       const params = {
         key: API_KEY,
         part: 'snippet',
@@ -49,7 +57,7 @@ export default {
         q: this.inputValue,
       }
 
-      //3. Youtube로 axios를 활용해 요청을 보낸다.
+      //5. Youtube로 axios를 활용해 요청을 보낸다.
       axios.get(API_URL, {
         // params: params,
         params,
@@ -58,13 +66,17 @@ export default {
         console.log(res)
         console.log(res.data.items)
       
-        //4. Youtube로부터 응답 받은 영상 목록(배열)을 배열에 넣는다.
+        //6. Youtube로부터 응답 받은 영상 목록(배열)을 배열에 넣는다.
         this.videos = res.data.items
       })
       .catch((err) => {
         console.log(err)
       })
-    }
+    },
+    // 15. 메서드가 실행되면 자식 컴포넌트로부터 올라온 video 데이터를 App 컴포넌트의 selectedVideo 데이터에 넣는다.  
+    onVideoSelect: function (video) {
+      this.selectedVideo = video
+    },
   }
 }
 </script>
--- 2.Web/5. Vue/01_sfc/youtube-project/src/components/SearchBar.vue
+++ 2.Web/5. Vue/01_sfc/youtube-project/src/components/SearchBar.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- 1. 검색어를 입력하고 enter를 누르면 'onInputKeyword'라는 메서드가 실행된다. -->
     <input @keypress.enter="onInputKeyword" type="text">
   </div>
 </template>
@@ -11,7 +12,7 @@ export default {
     onInputKeyword: function (event) {
       // console.log(event.target.value)
 
-      // 부모에게 event를 알려준다.(방출한다.) ('커스텀 이벤트', 넘겨줄 데이터)
+      // 2. onInputKeyword 메서드가 실행되면 부모 컴포넌트(App.vue)로 'input-change'라는 커스텀 이벤트와 데이터가 올라간다.
       this.$emit('input-change', event.target.value)
     }
   }
